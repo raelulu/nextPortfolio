6489130973c60cfddf19fa8ab41406d16f79629b
raelulu <raelulu23@gmail.com>
2023-11-10 19:42:33 +0900

    feat: create nextPortfolio

---
+++ portfolio/src/app/components/Contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <a href="/contact">Contact+</a>
+    </div>
+  );
+}
---
+++ portfolio/src/app/components/Skills.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div>
+      <a href="/skills">Skills</a>
+    </div>
+  );
+}
